d308e43032578f878651f021a90fc6e3914a9487
Adán G. Medrano-Chávez <ryuuba@gmail.com>
2024-05-08 10:51:04 -0600

    change dictionary type

--- fsm.py
+++ fsm.py
@@ -8,7 +8,7 @@ class FSM:
             initial_state (int): the code of the initial state (default = 0)
         """
         self.__current_state = initial_state
-        self.__transitions : dict[tuple[int, str], int] = {}
+        self.__transitions : dict[tuple[int, int], int] = {}
 
     def get_current_state(self) -> int:
         """ Returns the current state of the FSM
@@ -18,7 +18,7 @@ class FSM:
         """
         return self.__current_state
     
-    def set_transition_rule(self, current_state: int, event: str, next_state: int) -> None:
+    def set_transition_rule(self, current_state: int, event_number: int, next_state: int) -> None:
         """Set the transition rules the FSM obeys
 
         Args:
@@ -26,9 +26,9 @@ class FSM:
             event (str): the name of the interrupt that yields the state change
             next_state (int): the code of the next state
         """
-        self.__transitions[(current_state, event)] = next_state
+        self.__transitions[(current_state, event_number)] = next_state
 
-    def get_next_state(self, current_state: int, event: str) -> int:
+    def get_next_state(self, current_state: int, event_number: int) -> int:
         """ Returns the code of the next state
 
         Args:
@@ -38,13 +38,13 @@ class FSM:
         Returns:
             int: The code of the next state
         """
-        return self.__transitions[(current_state, event)]
+        return self.__transitions[(current_state, event_number)]
     
-    def compute_next_state(self, event: str) -> None:
+    def compute_next_state(self, event_number: int) -> None:
         """ Updates the code of the current state according to the code of the next state
 
         Args:
             event (str): the kind of interrupt producing the change of state
 
         """
-        self.__current_state = self.__transitions[(self.__current_state, event)]
+        self.__current_state = self.__transitions[(self.__current_state, event_number)]
