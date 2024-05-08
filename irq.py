dc5e1e85176b4a7a132faf71be4f4076e8e069f4
Adán G. Medrano-Chávez <ryuuba@gmail.com>
2024-05-08 09:58:51 -0600

    define press_button IRQ

--- irq.py
+++ irq.py
@@ -1,3 +1,7 @@
+from machine import Pin, Timer
+from fsm import FSM
+from utime import sleep_ms
+
 class IRQHandler:
     def __init__(self, fsm: FSM) -> None:
         """Overload constructor requiring the fsm that holds the transition rules. The methods here defined are ad hoc to a given microcontroller application
@@ -7,4 +11,16 @@ class IRQHandler:
         """
         self.__fsm = fsm
 
-    def 
+    def press_button(self, pin: Pin) -> None:
+        """ IRQ execute when the push button is pressed
+
+        Args:
+            pin (Pin): The IRQ requieres a pin to performed some action with it (not needed in all cases)
+        """
+        self.__fsm.compute_next_state('press_button')
+        # move to debounce button
+
+    
+    
+
+
