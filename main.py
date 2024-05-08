e42780e13e7a5d9a8ba92cfa841debbe4766c057
Adán G. Medrano-Chávez <ryuuba@gmail.com>
2024-05-08 12:08:05 -0600

    Add configuration of timer

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from machine import Pin, I2C
+from machine import Pin, I2C, Timer
 from ssd1306 import SSD1306_I2C
 from utime import sleep
 from digital_clock import DigitalClock
@@ -28,28 +28,31 @@ def main() -> None:
     fsm = FSM()
     event = {
         'unconditional' : 0, 
-        'none' : 1, 
+        'default' : 1, 
         'press button' : 2, 
         'timeout' : 3, 
         'button' : 4, 
         'not button' : 5}
     init_fsm(fsm, event)
-    irq = IRQHandler(fsm, event)
+    irq_handler = IRQHandler(fsm, event)
     clock = DigitalClock(23,59,50)
     led = Pin("LED", Pin.OUT)
     button = Pin(15, Pin.IN, Pin.PULL_UP)
-    button.irq(trigger=Pin.IRQ_RISING, handler=irq.press_button)
+    button.irq(trigger=Pin.IRQ_RISING, handler=irq_handler.press_button)
     i2c = I2C(0, sda=Pin(16), scl=Pin(17), freq=100000)
     oled = SSD1306_I2C(128,64,i2c)
+    t = Timer(1) # create a timer object using timer 1
+    t.init(mode=Timer.PERIODIC, period=1000, callback=irq_handler.timeout) # initialize it in periodic mode
     # unconditionally, passes the the next state 'cause init is done
     fsm.compute_next_state(event['unconditional'])
+    irq_state = disable_irq()
     while True:
         state = fsm.get_current_state()
         if state == 1:
             enable_irq(irq_state)
             fsm_actions.keep_alive(led, irq_state)
             fsm.compute_next_state(event['default'])
-            # Other changes of state are set by the interrupt handler
+            # Other events are controlled by the interrupt handler
         elif state == 2:
             irq_state = disable_irq()
             button_val = fsm_actions.debounce_button(button)
@@ -70,6 +73,7 @@ def main() -> None:
             fsm_actions.display_clock(clock, oled)
         else:
             fsm_actions.unknown_state()
+            break
             
 
 if __name__ == '__main__':
