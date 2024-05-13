4153829423d98f5fc9f25a54c64f790b27f9e698
Adán G. Medrano-Chávez <ryuuba@gmail.com>
2024-05-13 15:01:56 -0600

    Fix bug on RISING button

--- test.py
+++ test.py
@@ -7,6 +7,7 @@ from random import randint
 from utime import sleep
 from machine import I2C, Pin
 from ssd1306 import SSD1306_I2C
+from irq_handler import IRQHandler
 
 def test_fsm()->None:
     """This module evaluates the correct operation of FSM objects
@@ -51,7 +52,7 @@ def test_fsm()->None:
             break
         sleep(5)
 
-def test_fsm_with_oled()->None:
+def test_fsm_with_oled() -> None:
     """This module evaluates the correct operation of FSM objects
     """
     i2c = I2C(0, sda=Pin(16), scl=Pin(17), freq=400000)
@@ -76,6 +77,50 @@ def test_fsm_with_oled()->None:
                 fsm.compute_next_state(event['timeout'])
             elif rnd == 2:
                 fsm.compute_next_state(event['press button'])
+        elif state == 2:
+            oled.text(f'current state {state} = S2', 10, 0)
+            if randint(0, 1) == 0:
+                fsm.compute_next_state(event['not button'])
+            else:
+                fsm.compute_next_state(event['button'])
+        elif state == 3:
+            oled.text(f'current state {state} = S3', 10, 0)
+            fsm.compute_next_state(event['unconditional'])
+        elif state == 4:
+            oled.text(f'current state {state} = S4', 10, 0)
+            fsm.compute_next_state(event['unconditional'])
+        elif state == 5:
+            oled.text(f'current state {state} = S5', 10, 0)
+            fsm.compute_next_state(event['unconditional'])
+        else:
+            print(f'current state {state}')
+            break
+        oled.show()
+        sleep(5)
+        oled.fill(0)
+
+def test_fsm_with_button() -> None:
+    """This module evaluates the correct operation of FSM objects
+    """
+    fsm = FSM()
+    event = {
+        'unconditional' : 0, 
+        'default' : 1, 
+        'press button' : 2, 
+        'timeout' : 3, 
+        'button' : 4, 
+        'not button' : 5}
+    irq_handler = IRQHandler(fsm, event)
+    i2c = I2C(0, sda=Pin(16), scl=Pin(17), freq=400000)
+    oled = SSD1306_I2C(128,64,i2c)
+    button = Pin(15, Pin.IN, Pin.PULL_UP)
+    button.irq(trigger=Pin.IRQ_FALLING, handler=irq_handler.press_button)
+    init_fsm(fsm, event)
+    while True:
+        state = fsm.get_current_state()
+        if state == 1:
+            oled.text(f'current state {state} = S1', 10, 0)
+            fsm.compute_next_state(event['default'])
         elif state == 2:
             oled.text(f'current state {state} = S2', 10, 0)
             if randint(0, 1) == 0:
