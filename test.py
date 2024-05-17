d1e9f493e1e06a294d1571fd7e88b85459916a69
Adán G. Medrano-Chávez <ryuuba@gmail.com>
2024-05-16 20:36:58 -0600

    Redefine FSM

--- test.py
+++ test.py
@@ -2,18 +2,17 @@
 """
 
 from fsm import FSM
-from random import randint
 from utime import sleep, sleep_ms
 from machine import I2C, Pin, Timer, enable_irq, disable_irq
 from ssd1306 import SSD1306_I2C
 from irq_handler import press_button, timeout
-from fsm_actions import init_fsm, debounce_button
-from shared_obj import fsm, ev, digital_clock
+from fsm_actions import init_fsm, read_button
+import shared_obj
 
 def test_oled() -> None:
     i2c = I2C(0, sda=Pin(16), scl=Pin(17), freq=400000)
-    oled = SSD1306_I2C(128,32,i2c)
-    oled.text(f'Hola, Andre', 10, 10)
+    oled = SSD1306_I2C(128,64,i2c)
+    oled.text(f'You\'are safe here!!!', 0, 10)
     oled.show()
 
 
@@ -26,38 +25,30 @@ def test_fsm_interrupt() -> None:
     button.irq(trigger=Pin.IRQ_FALLING, handler=press_button, hard=True)
     t = Timer() # create a timer object using timer 1
     t.init(mode=Timer.PERIODIC, period=1000, callback=timeout)
-    init_fsm(fsm, ev)
-    fsm.compute_next_state(ev['unconditional'])
+    init_fsm(shared_obj.fsm, shared_obj.ev)
+    print(shared_obj.fsm.get_current_state())
+    shared_obj.fsm.compute_next_state(shared_obj.ev['unconditional'])
     print('init OK')
     while True:
-        state = fsm.get_current_state()
+        state = shared_obj.fsm.get_current_state()
         if state == 1:
-            print('S1: Waiting for an event...')
-            h, m, s = digital_clock.get_time()
-            oled.text(f'{h:02}:{m:02}:{s:02}', 0, 0)
+            h, m, s = shared_obj.digital_clock.get_time()
+            time = f'{h:02}:{m:02}:{s:02}'
+            oled.text(time, 0, 0)
             oled.show()
-            sleep(10)
+            if shared_obj.clear_display:
+                oled.fill(0)
+                shared_obj.clear_display = False
+            shared_obj.fsm.compute_next_state(shared_obj.ev['default'])
+            if shared_obj.debounce_button and read_button(button) == 0:
+                shared_obj.debounce_button = False
+                shared_obj.fsm.compute_next_state(shared_obj.ev['press button'])
         elif state == 2:
-            print('S2: debounce button')
-            interrupt_state = disable_irq()
-            sleep_ms(5)
-            button_val = debounce_button(button)
-            if button_val == 0:
-                fsm.compute_next_state(ev['button'])
-            else:
-                fsm.compute_next_state(ev['not button'])
-        elif state == 3:
-            digital_clock.clear_time()
-            fsm.compute_next_state(ev['unconditional'])
-        elif state == 4:
-            oled.fill(0)
-            enable_irq(interrupt_state)
-            fsm.compute_next_state(ev['unconditional'])
-        elif state == 5:
-            interrupt_state = disable_irq()
-            sleep_ms(5)
-            digital_clock.increment()
-            fsm.compute_next_state(ev['unconditional'])
+            # get time from server
+            irq_state = disable_irq()
+            shared_obj.digital_clock.clear_time()
+            enable_irq(irq_state)
+            shared_obj.fsm.compute_next_state(shared_obj.ev['unconditional'])
         else:
-            print(f'Are you OK, Any?')
+            print('Are you OK, Annie?')
             break
