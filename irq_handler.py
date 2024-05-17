acebc70208853ad076ec500b4ab103918f1cd1d8
Adán G. Medrano-Chávez <ryuuba@gmail.com>
2024-05-16 20:36:17 -0600

    Update ISR actions to not interfere FSM

--- irq_handler.py
+++ irq_handler.py
@@ -1,6 +1,6 @@
 from machine import Pin, Timer
 from fsm import FSM
-from shared_obj import fsm, ev
+import shared_obj
 
 
 def press_button(pin: Pin) -> None:
@@ -9,8 +9,7 @@ def press_button(pin: Pin) -> None:
     Args:
         pin (Pin): The IRQ requieres a pin to perform some action with it (not needed in all cases)
     """
-    # move to debounce button
-    fsm.compute_next_state(ev['press button'])
+    shared_obj.debounce_button = True
 
 def timeout(t : Timer) -> None:
     """IRQ executed when a timer rings
@@ -18,7 +17,8 @@ def timeout(t : Timer) -> None:
     Args:
         t (Timer): The IRQ requieres a timer to perform some action with it (not needed in all cases)
     """
-    fsm.compute_next_state(ev['timeout'])
+    shared_obj.digital_clock.increment()
+    shared_obj.clear_display = True
     
 
 
